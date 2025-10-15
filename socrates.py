4d1486d74efd1399932d1fc3a4a23ae741b2b32e
JackHarrisonUni <26527367@edgehill.ac.uk>
2025-10-15 22:51:58 +0100

    day 1 commit changed made include taking inout from users and creating a module list. this will chang as it is not scalable also need to add error handling for missinput as well as a kill switch for athena.

--- socrates.py
+++ socrates.py
@@ -1,5 +1,5 @@
 ### Variables for Socrates.net
-Ascii_Banner = """
+Ascii_Banner = r"""
   /$$$$$$                                           /$$                            /$$   /$$             /$$    
  /$$__  $$                                         | $$                           | $$$ | $$            | $$    
 | $$  \__/  /$$$$$$   /$$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$$   | $$$$| $$  /$$$$$$  /$$$$$$  
@@ -8,14 +8,24 @@ Ascii_Banner = """
  /$$  \ $$| $$  | $$| $$      | $$      /$$__  $$  | $$ /$$| $$_____/ \____  $$   | $$\  $$$| $$_____/  | $$ /$$
 |  $$$$$$/|  $$$$$$/|  $$$$$$$| $$     |  $$$$$$$  |  $$$$/|  $$$$$$$ /$$$$$$$//$$| $$ \  $$|  $$$$$$$  |  $$$$/
  \______/  \______/  \_______/|__/      \_______/   \___/   \_______/|_______/|__/|__/  \__/ \_______/   \___/  
- """
+"""
 
 version = "0.0.1"
 author = "Socrates"
 description = "A modular cybersecurity suit inpired by Philosophy and Logic. " # describe what the socrates suit does
+menu = """
+[1] --- plato.py ---------  Logic and reasoning structures ---
+[2] --- lyceum.py --------  Knowledge store (files/logs) -----
+[3] --- epictetus.py -----  Error handler --------------------
+[4] --- sentinel.py ------  Monitoring/logging ---------------
+[5] --- hermes.py --------  Network or message module --------
+[6] --- athena.py -------   Kill and exit --------------------
 
+Please select a module to load:     """
 
 
 
 ### Boot sequance
 print(f"{Ascii_Banner}\n\n\nVersion: {version} | Author: {author}\nDescription: {description}")
+tool_Selected = input(menu)
+print(f"You have selected option {tool_Selected}. Loading module...")
